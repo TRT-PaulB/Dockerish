ec950c3b4cda5240a5e03c433b44387a4801cf8c
TRT-PaulB <paul.bickell@raytheon.co.uk>
2020-03-23 15:03:59 +0000

    first commit

---
+++ src/main/java/com/example/Dockerish/DockerishApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dockerish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dockerish/DockerishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dockerish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
